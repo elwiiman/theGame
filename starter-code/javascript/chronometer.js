25c3502810e1ea4860ee5f51ad1a5c06da34a6cd
elwiiman <emmanuel.sanro@gmail.com>
2019-08-19 20:16:36 -0500

    Enfoque con tracking de posicion

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,68 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 100;
+    this.height = 120;
+    this.currentTime = 0;
+    this.intervalId;
+    this.instanceTimer = [];
+    this.instanceKeyPressed = [];
+    this.masterTimer = [0];
+    this.posTrackerXaxis = [];
+    this.posTrackerYaxis = [];
+    this.image = new Image();
+    this.image.src = "https://bit.ly/2L7yH3f";
+  }
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      this.masterTimer.push(this.currentTime);
+      this.posTrackerXaxis.push(this.x);
+      this.posTrackerYaxis.push(this.y);
+      this.draw();
+    }, 1000 / 1000);
+  }
+
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  // getMinutes() {
+  //   let minutes = 0;
+  //   minutes = Math.floor(this.currentTime / 60);
+  //   return minutes;
+  // }
+  // getSeconds() {
+  //   let seconds = 0;
+  //   seconds = this.currentTime % 60;
+  //   return seconds;
+  // }
+
+  // twoDigitsNumber(number) {
+  //   if (number.toString().length < 2) {
+  //     number = number.toString();
+  //     let finalNumber = "0" + number;
+  //     return finalNumber;
+  //   } else {
+  //     return number.toString();
+  //   }
+  // }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  splitClick() {
+    return this.currentTime;
+  }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,40 @@
-var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
-
-function printTime() {
-
+function getTimeAndKey(key) {
+  chronometer.instanceTimer.push(chronometer.currentTime);
+  chronometer.instanceKeyPressed.push(key);
 }
 
-function printMinutes() {
+//-----------------------------------------------
 
-}
+let isMasterTimerON = false;
+let chronometerInstanceArr = [];
+let chronometerCurrentInstance = 0;
+let chronometer = new Chronometer(50, 50);
 
-function printSeconds() {
-
-}
-
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
-
-// Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
-});
-
-// Reset/Split Button
-btnRight.addEventListener('click', function () {
-
-});
+document.onkeydown = function(e) {
+  // tecla enter
+  if (e.keyCode === 13) {
+    chronometer.startClick();
+    isMasterTimerON = true;
+    console.log("empieza el tiempo!");
+  } else if (e.keyCode === 37) {
+    // tecla left
+    getTimeAndKey("Left");
+    chronometer.x -= 7;
+  } else if (e.keyCode === 39) {
+    // tecla right
+    getTimeAndKey("Right");
+    chronometer.x += 7;
+  } else if (e.keyCode === 38) {
+    //tecla up
+    getTimeAndKey("Up");
+  } else if (e.keyCode === 40) {
+    //tecla down
+    getTimeAndKey("Down");
+  } else if (e.keyCode === 32) {
+    // tecla space
+    chronometer.resetClick();
+  } else if (e.keyCode === 83) {
+    // tecla s
+    chronometer.stopClick();
+  }
+};
